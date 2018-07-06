d8d0f6b0a4f89e66158812be8452cc8d8382fc2b
raman tehlan <ramantehlan@gmail.com>
2018-07-06 17:08:52 +0530

    Hero Class added

---
+++ src/lib/hero.ts
@@ -0,0 +1,12 @@
+/*
+*
+* This class is to give the structure to the Hero
+*
+* Author: Raman Tehlan
+* Date of creation: 06/07/2018
+*/ 
+
+export class Hero{
+    id: number;
+    name: string;
+}
