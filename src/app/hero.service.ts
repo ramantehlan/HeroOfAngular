cc55ce989357c7d4b5134ca2fb5d1251b439e894
raman tehlan <ramantehlan@gmail.com>
2018-07-11 15:04:46 +0530

    Message service connected to hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,6 +6,8 @@ import { Observable, of } from 'rxjs';
 import { Hero } from "../lib/hero";
 // Geting the demo information about the heroes
 import { HEROES } from "../lib/mock-heroes";
+//
+import { MessageService } from "./message.service";
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +17,13 @@ export class HeroService {
   // To pass the feroes list when requested
   // Observable is to make the process synchronous
   getHeroes(): Observable<Hero[]>{
+    // ToDo: send the message _after_fetched
+    this.messageService.add('HeroService: fetched heroesas a fkasdj fslkd akldfj asldf f jaslkdf aff aslkfd asfasfda akdf ad sd fasas flsaf  ljaslkf  f');
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) {
+
+   }
 }
