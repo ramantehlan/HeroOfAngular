33fd473db9f88189ca96c359114dc8ab6ee927a5
raman tehlan <ramantehlan@gmail.com>
2018-07-18 19:28:49 +0530

    Hero Details process changed

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -3,6 +3,12 @@
 import { Component, OnInit, Input} from '@angular/core';
 // To get the structure of the hero
 import { Hero } from '../../lib/hero';
+// To get the list of all the heroes from service 
+import { HeroService } from "../../service/hero.service";
+// To get information comming from the router
+import { ActivatedRoute } from '@angular/router';
+// To interact with the browser and to go back previous page
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
@@ -15,9 +21,19 @@ export class HeroDetailComponent implements OnInit {
   //it is fetched from heroes component
   @Input() hero: Hero;
   
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void{
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero)
   }
 
 }
