eb2998c9f074e31b21db9249dbe5991ad4e5da9b
raman tehlan <ramantehlan@gmail.com>
2018-07-19 14:43:09 +0530

    Hero Details updated

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -36,4 +36,8 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id).subscribe(hero => this.hero = hero)
   }
 
+  goBack(): void{
+    this.location.back();
+  }
+
 }
