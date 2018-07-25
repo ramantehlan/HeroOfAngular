b0c8d82068068a6979bb84382ae1553d7b0e825c
raman tehlan <ramantehlan@gmail.com>
2018-07-25 13:03:42 +0530

    in-memory-data service added | Implemented in hero.service.ts

--- src/service/hero.service.ts
+++ src/service/hero.service.ts
@@ -4,22 +4,30 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 // Getting the structure of hero object
 import { Hero } from "../lib/hero";
-// Geting the demo information about the heroes
+//
 import { HEROES } from "../lib/mock-heroes";
 // Adding the message service
 import { MessageService } from "./message.service";
+// To use the protocol
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 
   // To pass the feroes list when requested
   // Observable is to make the process synchronous
   getHeroes(): Observable<Hero[]>{
     // ToDo: send the message _after_fetched
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   // To fetch the hero with id 
@@ -28,7 +36,8 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   }
 
-  constructor(private messageService: MessageService) {
+  constructor(  private http: HttpClient,
+                private messageService: MessageService) {
 
    }
 }
